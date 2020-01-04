9eb942a4f7a8921cdcd80343229675d5164eb5b4
abir-chakroun <57041857+abir-chakroun@users.noreply.github.com>
2020-01-05 00:26:40 +0100

    Add files via upload

---
+++ build/precache-manifest.87c861ccbf0de9718ffecea82c4d3d00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a6f9dfe4041e085920548aa89e95bf",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "704fc89b3c5a24a0dae0",
+    "url": "/build/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8b807ee340f1b78ecfdb",
+    "url": "/build/static/css/main.e685b6bf.chunk.css"
+  },
+  {
+    "revision": "704fc89b3c5a24a0dae0",
+    "url": "/build/static/js/2.6ac2ca54.chunk.js"
+  },
+  {
+    "revision": "a5a8b7af208d27ffe80a0e6f18f394d1",
+    "url": "/build/static/js/2.6ac2ca54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b807ee340f1b78ecfdb",
+    "url": "/build/static/js/main.f13f4731.chunk.js"
+  },
+  {
+    "revision": "369204c2dab4ff1f4080",
+    "url": "/build/static/js/runtime-main.f1624457.js"
+  },
+  {
+    "revision": "d3007d2809b8be7a7747ec7507e7965e",
+    "url": "/build/static/media/logo.d3007d28.png"
+  },
+  {
+    "revision": "06ea83c32a7c2f5ba80770a4645acb43",
+    "url": "/build/static/media/special.06ea83c3.jpg"
+  }
+]);
